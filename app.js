ac3a2d304ff3520bf7903d34192707ae4d311faf
nasimuddin01 <debater.nasim@gmail.com>
2019-12-23 14:46:18 +0600

    first mvp

---
+++ app.js
@@ -0,0 +1,45 @@
+fetch("http://api.open-notify.org/iss-now.json")
+  .then(res => res.json())
+  .then(data => data.iss_position)
+  .then(data => {
+
+    let x = parseFloat(data.latitude);
+    let y = parseFloat(data.longitude);
+    console.log(x, y);
+
+
+    var baseMapLayer = new ol.layer.Tile({
+      source: new ol.source.OSM()
+    });
+    var map = new ol.Map({
+      target: 'map',
+      layers: [baseMapLayer],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([y, x]),
+        zoom: 4 //Initial Zoom Level
+      })
+    });
+
+    var marker = new ol.Feature({
+      geometry: new ol.geom.Point(
+        ol.proj.fromLonLat([y, x])
+      ), // Cordinates of ISS
+    });
+
+    marker.setStyle(new ol.style.Style({
+      image: new ol.style.Icon( ({
+        color: '#ffcd46',
+        crossOrigin: '',
+        src: 'https://raw.githubusercontent.com/itsmohitt/openlayers-tutorials/master/dot.png'
+      }))
+    }));
+
+    var vectorSource = new ol.source.Vector({
+      features: [marker]
+    });
+    var markerVectorLayer = new ol.layer.Vector({
+      source: vectorSource,
+    });
+    map.addLayer(markerVectorLayer);
+
+  });
